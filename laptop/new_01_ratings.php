493528e0689187c8ce0c2f8869b6561e2fe3fce2
VanchoRancho <ibaranov86@gmail.com>
2017-02-12 11:05:49 +0300

    rating page [full] & start for partners page

---
+++ laptop/new_01_ratings.php
@@ -0,0 +1,132 @@
+<?php include_once 'header.inc.php';?>
+
+    <h1 class="rating_title">Рейтинг</h1>
+
+    <div class="tabs ratings_category">
+        <ul class="dib">
+            <li class="dib"><a href="#">Ресторанного Гида</a></li>
+            <li class="dib active"><a href="#">Пользователей</a></li>
+        </ul>
+    </div>
+
+    <aside class="dib pad_r_30">
+        <ul class="list_links_to_pages category_links">
+            <li class="first"><span class="color_middle">Списки</span></li>
+            <li><a href="#">Итальянские рестораны</a></li>
+            <li><a href="#">Восточные рестораны</a></li>
+            <li><a href="#">Кавказские рестораны</a></li>
+            <li><a href="#">Русские рестораны</a></li>
+            <li><a href="#" class="active">Итальянские рестораны</a></li>
+            <li><a href="#">Недорогие рестораны</a></li>
+            <li><a href="#">Вегетарианские кафе</a></li>
+            <li><a href="#">Винные рестораны</a></li>
+            <li><a href="#">Итальянские рестораны</a></li>
+        </ul>
+    </aside>
+    <div class="dib row-n">
+
+      <div class="rating_category">
+        <h1>Итальянские рестораны</h1>
+        <p>
+          Лучшие итальянские рестораны, где вам с радостью предложат и оливье, и шаверму, и котлеты - всё без мяса! Напитки также с исключительно полезным подтекстом.
+        </p>
+
+      </div>
+
+      <div class="list_rating">
+
+        <div class="item dib">
+            <a href="#" class="img_holder">
+              <span class="place"></span>
+              <img src="img/content/10_blog_articles/articles_cover_01.jpg" alt="">
+            </a>
+            <div class="params">
+                <a href="#" class="h1">Суши-бистро Cutfish</a>
+                <div class="param">
+                  <div class="dib hidden" data-title="Рейтинг Ресторанного Гида"><img src="img/rating_rg.svg" class="dibm" alt=""><i class="dibm">7,6</i></div>
+                  <div class="dib" data-title="Рейтинг пользователей"><img src="img/rating_users.svg" class="dibm" alt=""><i class="dibm">7,3</i></div>
+                </div>
+            </div>
+            <p>У заработавшего в начале мая Cutfish буквально звёздный состав основателей — место принадлежит Александру Оганезову и Глену Баллису.</p>
+        </div>
+
+        <div class="item dib">
+            <a href="#" class="img_holder">
+              <span class="place"></span>
+              <img src="img/content/10_blog_articles/articles_cover_01.jpg" alt="">
+            </a>
+            <div class="params">
+                <a href="#" class="h1">Суши-бистро Cutfish</a>
+                <div class="param">
+                  <div class="dib hidden" data-title="Рейтинг Ресторанного Гида"><img src="img/rating_rg.svg" class="dibm" alt=""><i class="dibm">7,6</i></div>
+                  <div class="dib" data-title="Рейтинг пользователей"><img src="img/rating_users.svg" class="dibm" alt=""><i class="dibm">7,3</i></div>
+                </div>
+            </div>
+            <p>У заработавшего в начале мая Cutfish буквально звёздный состав основателей — место принадлежит Александру Оганезову и Глену Баллису.</p>
+        </div>
+
+        <div class="item dib">
+            <a href="#" class="img_holder">
+              <span class="place"></span>
+              <img src="img/content/10_blog_articles/articles_cover_01.jpg" alt="">
+            </a>
+            <div class="params">
+                <a href="#" class="h1">Суши-бистро Cutfish</a>
+                <div class="param">
+                  <div class="dib hidden" data-title="Рейтинг Ресторанного Гида"><img src="img/rating_rg.svg" class="dibm" alt=""><i class="dibm">7,6</i></div>
+                  <div class="dib" data-title="Рейтинг пользователей"><img src="img/rating_users.svg" class="dibm" alt=""><i class="dibm">7,3</i></div>
+                </div>
+            </div>
+            <p>У заработавшего в начале мая Cutfish буквально звёздный состав основателей — место принадлежит Александру Оганезову и Глену Баллису.</p>
+        </div>
+
+        <div class="item dib">
+            <a href="#" class="img_holder">
+              <span class="place"></span>
+              <img src="img/content/10_blog_articles/articles_cover_01.jpg" alt="">
+            </a>
+            <div class="params">
+                <a href="#" class="h1">Суши-бистро Cutfish</a>
+                <div class="param">
+                  <div class="dib hidden" data-title="Рейтинг Ресторанного Гида"><img src="img/rating_rg.svg" class="dibm" alt=""><i class="dibm">7,6</i></div>
+                  <div class="dib" data-title="Рейтинг пользователей"><img src="img/rating_users.svg" class="dibm" alt=""><i class="dibm">7,3</i></div>
+                </div>
+            </div>
+            <p>У заработавшего в начале мая Cutfish буквально звёздный состав основателей — место принадлежит Александру Оганезову и Глену Баллису.</p>
+        </div>
+
+        <div class="item dib">
+            <a href="#" class="img_holder">
+              <span class="place"></span>
+              <img src="img/content/10_blog_articles/articles_cover_01.jpg" alt="">
+            </a>
+            <div class="params">
+                <a href="#" class="h1">Суши-бистро Cutfish</a>
+                <div class="param">
+                  <div class="dib hidden" data-title="Рейтинг Ресторанного Гида"><img src="img/rating_rg.svg" class="dibm" alt=""><i class="dibm">7,6</i></div>
+                  <div class="dib" data-title="Рейтинг пользователей"><img src="img/rating_users.svg" class="dibm" alt=""><i class="dibm">7,3</i></div>
+                </div>
+            </div>
+            <p>У заработавшего в начале мая Cutfish буквально звёздный состав основателей — место принадлежит Александру Оганезову и Глену Баллису.</p>
+        </div>
+
+        <div class="item dib">
+            <a href="#" class="img_holder">
+              <span class="place"></span>
+              <img src="img/content/10_blog_articles/articles_cover_01.jpg" alt="">
+            </a>
+            <div class="params">
+                <a href="#" class="h1">Суши-бистро Cutfish</a>
+                <div class="param">
+                  <div class="dib hidden" data-title="Рейтинг Ресторанного Гида"><img src="img/rating_rg.svg" class="dibm" alt=""><i class="dibm">7,6</i></div>
+                  <div class="dib" data-title="Рейтинг пользователей"><img src="img/rating_users.svg" class="dibm" alt=""><i class="dibm">7,3</i></div>
+                </div>
+            </div>
+            <p>У заработавшего в начале мая Cutfish буквально звёздный состав основателей — место принадлежит Александру Оганезову и Глену Баллису.</p>
+        </div>
+
+      </div>
+
+    </div>
+
+<?php include_once 'footer.inc.php';?>
---
+++ laptop/new_02_for_partners.php
@@ -0,0 +1,19 @@
+<?php include_once 'header.inc.php';?>
+
+    <h1>Добавьте свой ресторан на сайт</h1>
+
+    <div class="cols rest_add">
+      <div class="col-2 dib">
+        Заполните анкету и после подтверждения нашими менеджерами, ресторан будет добавлен.
+      </div>
+      <div class="col-2 dib">
+        <div class="add_btn dib">
+          Добавить ресторан
+        </div>
+      </div>
+    </div>
+
+    <p class="partners_title">
+      <span class="logo">Ресторанныйгид.рф</span> — удобный ресурс для продвижения Вашего ресторана
+    </p>
+<?php include_once 'footer.inc.php';?>
