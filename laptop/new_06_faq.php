554efca6c1784866132efc77fb827088438da4d8
VanchoRancho <ibaranov86@gmail.com>
2017-02-19 19:48:54 +0300

    add faq page and some style changes in agreement page

---
+++ laptop/new_06_faq.php
@@ -0,0 +1,50 @@
+<?php include_once 'header.inc.php';?>
+
+  <div class="faq_page">
+    <div class="faq_page_item">
+      <h1 class="faq_page_main-title">Часто задаваемые вопросы</h1>
+      <div class="faq_page_text">
+
+      </div>
+    </div>
+
+    <div class="faq_page_item">
+      <h3 class="faq_page_title dib accordion">Здесь заданный вопрос</h3>
+      <div class="faq_page_text">
+        <p>Здесь ответ на вопрос. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+
+    <div class="faq_page_item">
+      <h3 class="faq_page_title accordion dib">Здесь заданный вопрос</h3>
+      <div class="faq_page_text">
+        <p>Здесь ответ на вопрос. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+
+    <div class="faq_page_item">
+      <h3 class="faq_page_title accordion dib">Здесь заданный вопрос</h3>
+      <div class="faq_page_text">
+        <p>Здесь ответ на вопрос. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+
+    <div class="faq_page_item">
+      <h3 class="faq_page_title accordion dib">Здесь заданный вопрос</h3>
+      <div class="faq_page_text">
+        <p>Здесь ответ на вопрос. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+
+    <div class="faq_page_item">
+      <h3 class="faq_page_title accordion dib">Здесь заданный вопрос</h3>
+      <div class="faq_page_text">
+        <p>Здесь ответ на вопрос. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+
+  </div>
+
+  <script src="js/accordion.js"></script>
+
+<?php include_once 'footer.inc.php';?>
